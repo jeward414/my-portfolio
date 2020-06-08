e775e1fe28d7ac6c98c199167b65de2f8cf2fa1b
Jonathan <jeward414@github.com>
2020-06-08 17:47:42 +0000

    Add global variables, comment object, and timestamp

---
+++ portfolio/src/main/java/com/google/sps/commentdata/Comment.java
@@ -0,0 +1,23 @@
+package com.google.sps.commentdata;
+
+import java.util.Date;
+
+public final class Comment {
+
+    private String name;
+    private String comment;
+    private Date date;
+
+    public Comment() {
+        this.name = "";
+        this.comment = "";
+        this.date = null;
+    }
+
+    public Comment(String name, String comment, Date date) {
+        this.name = name;
+        this.comment = comment;
+        this.date = date;
+    }
+
+}
--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -28,11 +28,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import com.google.sps.commentdata.Comment;
 
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
-  private final ArrayList<String> comments = new ArrayList<String>();
   private static final Gson gson = new Gson();
+  private static final String COMMENT_FIELD = "comment-field";
+  private static final String NAME_FIELD = "name-field";
+  private static final String DATE_FIELD = "timestampValue";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -41,9 +46,13 @@ public class DataServlet extends HttpServlet {
     DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = dataStore.prepare(allComments);
 
-    ArrayList<String> commentList = new ArrayList<>();
+    List<Comment> commentList = new ArrayList<>();
     for (Entity entity : results.asIterable()) {
-        final String comment = (String) entity.getProperty("comment-field");
+        final String name = (String) entity.getProperty(NAME_FIELD);
+        final String text = (String) entity.getProperty(COMMENT_FIELD);
+        final Date date = (Date) entity.getProperty(DATE_FIELD);
+
+        Comment comment = new Comment(name, text, date);
 
         commentList.add(comment);
     }
@@ -52,29 +61,22 @@ public class DataServlet extends HttpServlet {
     response.getWriter().println(convertToJsonUsingGson(commentList));
   }
 
-  private String convertToJsonUsingGson(ArrayList<String> jsonData) {
+  private String convertToJsonUsingGson(List<Comment> jsonData) {
       return gson.toJson(jsonData);
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
       StringBuffer sb = new StringBuffer();
-      String text = getParameter(request, "comment-field", "");
-
-      String[] words = text.split(" ");
-      
-      for (String txt:words) {
-          comments.add(txt);
-      }
-
-      for (String s : comments) {
-          sb.append(s);
-          sb.append(" ");
-      }
+      String name = getParameter(request, NAME_FIELD, "");
+      String text = getParameter(request, COMMENT_FIELD, "");
+      Date timestamp = new Date();
 
       String comment = request.getParameter("comment-field");
       Entity taskEntity = new Entity("Comment");
-      taskEntity.setProperty("comment-field", comment);
+      taskEntity.setProperty(NAME_FIELD, name);
+      taskEntity.setProperty(COMMENT_FIELD, comment);
+      taskEntity.setProperty(DATE_FIELD, timestamp);
       DatastoreService dataStore = DatastoreServiceFactory.getDatastoreService();
       dataStore.put(taskEntity);
       
